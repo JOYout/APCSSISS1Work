db1a3c36cfc3cea9ed8029ca6aa134594e44f3f6
JOYout <118493976+JOYout@users.noreply.github.com>
2024-10-24 14:25:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        try {
+          File DB = new File("DB.csv");
+        if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+          } else {
+            System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
 
+        }
     }
 }
