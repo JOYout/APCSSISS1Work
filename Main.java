3df20cde71e05d8599b910e9df0cf82d10caa0ef
JOYout <118493976+JOYout@users.noreply.github.com>
2024-10-25 13:44:56 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,19 +4,26 @@ import java.io.IOException;  // Import the IOException class to handle errors
 
 public class Main
 {
+    Path CSV_File_Name = path/to/file;
+    List<String[]> dataLines = new ArrayList<>();
+
+    
     public static void main(String[] args)
     {
-        try {
-          File DB = new File("DB.csv");
-        if (myObj.createNewFile()) {
-            System.out.println("File created: " + myObj.getName());
-          } else {
-            System.out.println("File already exists.");
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+        dataLines.add(new String[] 
+          { "John", "Doe", "38", "Comment Data\nAnother line of comment data" });
+        dataLines.add(new String[] 
+          { "Jane", "Doe, Jr.", "19", "She said \"I'm being quoted\"" });
 
+        }
+    public void givenDataArray_whenConvertToCSV_thenOutputCreated() throws IOException {
+            File csvOutputFile = new File(CSV_FILE_NAME);
+            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+                dataLines.stream()
+                  .map(this::convertToCSV)
+                  .forEach(pw::println);
+            }
+            assertTrue(csvOutputFile.exists());
         }
     }
 }
