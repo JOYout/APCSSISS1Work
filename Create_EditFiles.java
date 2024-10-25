5e5b233f6ac6540122d1b8959d2a7de9646e9749
JOYout <118493976+JOYout@users.noreply.github.com>
2024-10-25 13:50:30 -0600

    Update Create_EditFiles.java

--- Create_EditFiles.java
+++ Create_EditFiles.java
@@ -4,10 +4,22 @@ import java.io.IOException;  // Import the IOException class to handle errors
 
 public class Create_EditFiles
 {
-public String convertToCSV(String[] data) {
-    return Stream.of(data)
-      .map(this::escapeSpecialCharacters)
-      .collect(Collectors.joining(","));
-}
+    public String convertToCSV(String[] data) {
+        return Stream.of(data)
+          .map(this::escapeSpecialCharacters)
+          .collect(Collectors.joining(","));
+    }
 
+    public void givenDataArray_whenConvertToCSV_thenOutputCreated() throws IOException {
+            File csvOutputFile = new File(CSV_FILE_NAME);
+            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+                dataLines.stream()
+                  .map(this::convertToCSV)
+                  .forEach(pw::println);
+            }
+            assertTrue(csvOutputFile.exists());
+        }
+
+    
+    
 }
