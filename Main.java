697f09e9510c141a94c612dc5cf0a7cacb294b01
JOYout <118493976+JOYout@users.noreply.github.com>
2024-10-25 13:49:21 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,14 +16,5 @@ public class Main
           { "Jane", "Doe, Jr.", "19", "She said \"I'm being quoted\"" });
 
         }
-    public void givenDataArray_whenConvertToCSV_thenOutputCreated() throws IOException {
-            File csvOutputFile = new File(CSV_FILE_NAME);
-            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-                dataLines.stream()
-                  .map(this::convertToCSV)
-                  .forEach(pw::println);
-            }
-            assertTrue(csvOutputFile.exists());
-        }
     }
 }
